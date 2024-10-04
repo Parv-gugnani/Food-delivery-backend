537f7c8cd174208fadabd92a858174d746251013
Parv-gugnani <9765.parv@gmail.com>
2024-10-04 17:51:46 +0530

    starting

--- Models/order.js
+++ Models/order.js
@@ -2,11 +2,6 @@ const express = require("express");
 const { default: mongoose } = require("mongoose");
 
 const OrderSchema = mongoose.Schema({
-  orderId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
   orderLocation: {
     type: String,
     required: true,
@@ -14,5 +9,8 @@ const OrderSchema = mongoose.Schema({
   foodName: {
     type: String,
     required: true,
+  },
+  payment: {
+    type: Boolean,
   },
 });
