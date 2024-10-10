dacd2f24683fe1bb523bbed6a094756386315ae0
Parv-gugnani <9765.parv@gmail.com>
2024-10-10 12:56:49 +0530

    id err

--- controller/userController.js
+++ controller/userController.js
@@ -7,28 +7,23 @@ const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
 };
 
-// Register User
 const registerUser = asyncHandler(async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
-  // Check for required fields
   if (!firstName || !lastName || !email || !password) {
     res.status(400);
     throw new Error("Please fill all fields");
   }
 
-  // Check if user already exists
   const userExist = await User.findOne({ email });
   if (userExist) {
     res.status(400);
     throw new Error("User Already Exist");
   }
 
-  // Hash the password
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
-  // Create the user with minimal details
   const user = await User.create({
     firstName,
     lastName,
@@ -50,7 +45,6 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-// Login User
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
@@ -78,24 +72,23 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const updateProfile = asyncHandler(async (req, res) => {
-  const { address, phoneNumber } = req.body;
   const userId = req.user._id;
+  const updates = {};
 
-  // Update the user with additional details
-  const updatedUser = await User.findByIdAndUpdate(
-    userId,
-    { address, phoneNumber },
-    { new: true, runValidators: true }
-  );
+  // Loop through request body and prepare updates
+  Object.keys(req.body).forEach((key) => {
+    if (req.body[key] !== undefined) {
+      updates[key] = req.body[key];
+    }
+  });
+
+  const updatedUser = await User.findByIdAndUpdate(userId, updates, {
+    new: true,
+    runValidators: true,
+  });
 
   if (updatedUser) {
-    res.status(200).json({
-      firstName: updatedUser.firstName,
-      lastName: updatedUser.lastName,
-      email: updatedUser.email,
-      address: updatedUser.address,
-      phoneNumber: updatedUser.phoneNumber,
-    });
+    res.status(200).json(updatedUser);
   } else {
     res.status(404);
     throw new Error("User not found");
